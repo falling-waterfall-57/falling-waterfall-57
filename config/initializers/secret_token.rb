8e9872e8e0b210f5ac2ae2ddc86caee6c38f29b1
“Kristine <kristine.robison@zvents.com>
2010-10-03 19:54:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modalware::Application.config.secret_token = '14c00f72d88b36b0aef58ef6f90c02f12d0e0b4a4199dd67b51ed867dc8a4ce454ceb90282f7fd1886ba3268e41245710865562466fc81f2738b7d8d45b8e85e'
